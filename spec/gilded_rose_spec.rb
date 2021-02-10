8636ed2d59a64d9f9d3aa13d437f0fa41f20f5d1
Ian Fleming <>
2021-02-10 16:35:55 +0000

    more passing tests

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -20,6 +20,34 @@ describe GildedRose do
       expect(items[0].quality).to eq 19
     end
 
+    it 'reduces item sell by date by 1 if the item is not brie or backstage passes' do
+      items = [Item.new('foo', 12, 20)]
+      GildedRose.new(items).update_quality
+      expect(items[0].sell_in).to eq 11
+    end
+
+    it 'reduces the quality by 1 for multiple items' do
+      items = [Item.new('foo', 12, 20)]
+      items << Item.new('ham', 15, 25)
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq 19
+      expect(items[1].quality).to eq 24
+    end
+
+    it 'reduces the sell by date by 1 for multiple items' do
+      items = [Item.new('foo', 12, 20)]
+      items << Item.new('ham', 15, 25)
+      GildedRose.new(items).update_quality
+      expect(items[0].sell_in).to eq 11
+      expect(items[1].sell_in).to eq 14
+    end
+
+    it 'reduces quality twice as fast when the sell by date has passed' do
+      items = [Item.new('foo', -1, 20)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq 18
+    end
+
     it 'does not change the name' do
       items = [Item.new('foo', 0, 0)]
       GildedRose.new(items).update_quality
